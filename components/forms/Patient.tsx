4ad8ac92130fe24413dbbc5e173544b738634684
Saurav Doke <sauravdoke@gmail.com>
2025-01-17 17:01:45 +0530

    Appwrite setup  , user creation

--- components/forms/Patient.tsx
+++ components/forms/Patient.tsx
@@ -19,6 +19,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 
 export enum FormFieldType {
@@ -51,16 +52,17 @@ export function Patient() {
     // console.log(values);
     setIsLoading(true);
     try {
-      // const userData = {
-      //   name,
-      //   email,
-      //   phone
-      // };
-      // const user = await createUser(userData)
-      // if(user) router.push(`/patient/${user.$id}/register`)
+      const userData = {
+        name,
+        email,
+        phone
+      };
+      const user = await createUser(userData)
+      if(user) router.push(`/patient/${user.$id}/register`)
     } catch (error) {
       console.log(error);
     }
+    setIsLoading(false);
   }
   return (
     <Form {...form}>
